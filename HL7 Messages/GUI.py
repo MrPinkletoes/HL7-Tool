52b9c8188a50f344b5c963fbef0540b854a1fadc
MrPinkletoes <lloyd.broley@gmail.com>
2017-10-16 12:07:08 +0100

    Add files via upload

--- HL7 Messages/GUI.py
+++ HL7 Messages/GUI.py
@@ -1,31 +1,50 @@
 #!/usr/bin/python
+import os
 from tkinter import *
+from HL7_Message_Creation import *
 
-def topten ():
-    print("Files Out")
-def cancel():
-    print("Cancelled")
-def cdown():
-    print("Database Cleared")
-def close():
-    root.destroy()
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+        # changing the title of our master widget
+        self.master.title("HL7 Tool")
+
+        # allowing the widget to take the full space of the root window
+        self.pack(fill=BOTH, expand=0)
+
+        def topten():
+            print("Printed")
+            #os.system("python topten.py")
+
+        def cancel():
+            print("Cancelled")
+
+        def cdown():
+            os.system("python C:\\Users\\lbroley\\PycharmProjects\\Sample360 Cleardowns\S360_Cleardown.py ")
+
+        def close():
+            root.destroy()
+
+        # Buttons
+        confirm = Button(root, text="Confirm", command=topten)
+        cancel = Button(root, text="Cancel", command=cancel)
+        cdown = Button(root, text="Clear database", command=cdown)
+        close = Button(root, text="Close", command=close)
+
+        cdown.place(x=163, y=570)
+        confirm.place(x=50, y=570)
+        cancel.place(x=110, y=570)
+        close.place(x=258, y=570)
 
 
 root = Tk()
-root.title("HL7 Message Tool")
-frame = Frame(root, bg="yellow", width="200", height="200")
-frame.grid()
-
-# Buttons
-confirm = Button(root, text="Confirm", bg="green", fg="white", command=topten)
-cancel = Button(root, text="Cancel", bg="red", fg="white", command=cancel)
-cdown = Button(root, text= "Clear database", bg="blue", fg="white", command=cdown)
-close = Button(root, text= "Close", bg="blue", fg="white", command=close)
-
-cdown.grid(row=0, column=3)
-confirm.grid(row=0)
-cancel.grid(row=0, column=1)
-close.grid(row=0, column=2)
-
+root.resizable(width=False, height=False)
+root.geometry("600x600")
+app = Window(root)
 
 root.mainloop()
