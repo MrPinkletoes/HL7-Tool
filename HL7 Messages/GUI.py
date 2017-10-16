e546e27e9eb7946e8bd21bb2efbabe1c60e431c0
MrPinkletoes <lloyd.broley@gmail.com>
2017-10-16 16:56:10 +0100

    Add files via upload

--- HL7 Messages/GUI.py
+++ HL7 Messages/GUI.py
@@ -17,6 +17,33 @@ class Window(Frame):
         # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=0)
 
+        def hide_list(event):
+            event.widget.pack_forget()
+
+        # Multirequest patient list
+        def Plist():
+            if CheckVar1.get() == 1:
+                Lb1 = Listbox(selectmode=SINGLE, height=5, yscrollcommand=1)
+                Lb1.insert(1, "Winnie Franks")
+                Lb1.insert(2, "Joan Roach")
+                Lb1.insert(3, "Marie Evans")
+                Lb1.insert(4, "Sally Jones")
+                Lb1.insert(5, "John Foulkes")
+                Lb1.insert(6, "Ian Potter")
+                Lb1.insert(7, "Barry Birch")
+                Lb1.insert(8, "Peter Parker")
+                Lb1.insert(9, "Bill Sykes")
+                Lb1.insert(10, "Mary Celeste")
+                Lb1.place(x=40, y=100)
+            elif CheckVar1.get() == 0:
+                Lb1 = Listbox()
+                Lb1.bind('<Button-1>', hide_list())
+                Lb2 = Listbox(selectmode=SINGLE, height=1, yscrollcommand=0)
+                Lb2.insert(1, "All")
+                Lb2.place(x=40, y=100)
+
+
+
         def topten():
             print("Printed")
             #os.system("python topten.py")
@@ -46,19 +73,9 @@ class Window(Frame):
         L1.place(x=10, y=20)
         E1 = Entry(bd=2)
         E1.place(x=185, y=18)
-        Lb1 = Listbox(root)
-        Lb1.insert(1, "Python")
-        Lb1.insert(2, "Perl")
-        Lb1.insert(3, "C")
-        Lb1.insert(4, "PHP")
-        Lb1.insert(5, "JSP")
-        Lb1.insert(6, "Ruby")
-        Lb1.place(x=10, y=50)
         CheckVar1 = IntVar()
-        C1 = Checkbutton(text="Muti request", variable=CheckVar1, onvalue=1, offvalue=0, height=5, width=5)
-        C1.place(x=50, y=50)
-
-
+        C1 = Checkbutton(text="Muti request", variable=CheckVar1, onvalue=1, offvalue=0, height=5, width=20, command=Plist)
+        C1.place(x=-20, y=50)
 
 
 root = Tk()
